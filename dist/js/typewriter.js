d0c6526ec44550f1bfc74a4f62c0bed66b3304e4
ZRShamim <zrshamim8822@gmail.com>
2021-06-21 15:26:15 +0600

    initial update

--- dist/js/typewriter.js
+++ dist/js/typewriter.js
@@ -63,3 +63,24 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(`.reveal`, {
+  interval: 200,
+});
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+  else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
